ad0fadc9ac1f707d222052aea2ad0379da294813
vampyteuth <94151404+vampyteuth@users.noreply.github.com>
2022-04-03 18:04:57 -0800

    Created dogyears

---
+++ dogyears.js
@@ -0,0 +1,19 @@
+let myAge = 42
+//how old I am
+let earlyYears = 2
+//represents the first two years of a dog's life
+earlyYears *=10.5
+laterYears = myAge -2
+//to even up for the 2 years we accounted for already
+laterYears *= 4
+//each of your years is 4 dog years
+
+console.log(earlyYears, laterYears)
+
+myAgeInDogYears = earlyYears + laterYears
+//total years incorporated into dog years
+
+myName = 'Terra'.toLowerCase()
+//my name in all lower case letters
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
